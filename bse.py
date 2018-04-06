ca9c18d988026d752223aef3220bd2b1ec3b1895
rohitrajiit <rohitraj.iitm@gmail.com>
2018-04-06 23:09:06 +0530

    Create bse.py

---
+++ bse.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  1 23:28:18 2018
+
+@author: rohit
+downloads scripwise price volume data
+"""
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+path = r'C:\Users\rohit\Downloads'
+
+driver = webdriver.Chrome()
+driver.get('https://www.bseindia.com/markets/equity/EQReports/StockPrcHistori.aspx?scripcode=512289&flag=sp&Submit=G')
+try:
+    fromdate = driver.find_element_by_id('ctl00_ContentPlaceHolder1_txtFromDate')
+    todate = driver.find_element_by_id('ctl00_ContentPlaceHolder1_txtToDate')
+    scrip = driver.find_element_by_id('ctl00_ContentPlaceHolder1_GetQuote1_smartSearch')
+    fromdate.clear()
+    fromdate.send_keys('12/03/2018')
+    driver.execute_script("document.getElementById('ctl00_ContentPlaceHolder1_txtToDate').value = '16/03/2018'")
+    scrip.clear()
+    scrip.send_keys('RCOM')
+    time.sleep(10)
+    scrip.send_keys(Keys.RETURN)
+    submitt= driver.find_element_by_id("ctl00_ContentPlaceHolder1_btnSubmit")
+    submitt.click()
+    download = driver.find_element_by_id("ctl00_ContentPlaceHolder1_btnDownload1")
+    download.click()
+    scripname = driver.find_element_by_id("ctl00_ContentPlaceHolder1_lblScripCodeValue").text
+    name = scripname + '.csv'
+    while True:
+        if name in os.listdir(path):
+            break
+        time.sleep(1)
+except:
+    print(sys.exc_info()[0])
+    
+    
+finally:
+    driver.close()
